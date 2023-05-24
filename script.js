967b7710e87dc2ced691fd27e6e854a4353b6897
MyUsernameDenisTrimignogit config --global user.email my@email.comdenis@trimigno.it <denis@trimigno.it>
2023-05-24 16:54:54 +0200

    esercizio 2/2

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-
-
-
 const emails = [
     'luca@live.it',
     'andrea@live.it',
@@ -23,3 +19,44 @@ if (emailFound === true) {
 } else {
     console.log ('email non trovata')
 }
+
+
+// esercizio 2 
+
+let risultati = [];
+let vittorieGiocatore = '';
+let vittorieComputer = '';
+
+for( let i = 0; i < 5 ; i++) {
+
+const giocatore = Math.floor(Math.random() * 6) + 1;
+const computer = Math.floor(Math.random() * 6) + 1;
+
+console.log(`giocatore: ${giocatore}, computer: ${computer}`);
+
+if(giocatore > computer) {
+    console.log('giocatore vince');
+    risultati.push('giocatore');
+} else if (giocatore < computer) {
+    console.log('compluter vince')
+    risultati.push('computer');
+} else {
+    console.log('patta')
+    risultati.push('patta')
+}
+
+}
+
+console.log(risultati);
+
+for(let i = 0; i < risultati.length; i++) {
+
+    if(risultati[i] === 'giocatore') {
+        vittorieGiocatore++;
+    } else if (risultati[i] === 'computer') {
+        vittorieComputer++;
+    }
+}
+
+console.log('vittorieGiocatore -->' , vittorieGiocatore);
+console.log('vittorieComputer -->', vittorieComputer);
